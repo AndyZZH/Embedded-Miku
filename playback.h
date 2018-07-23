5ec0839d7ac3ba3839c8f13ed9e6c03eca1dfcea
Peter Tan <pj.t@outlook.com>
2018-07-22 20:41:27 -0700

    Finish implementation of playback module

--- playback.h
+++ playback.h
@@ -1,4 +1,6 @@
 #ifndef __PLAYBACK_H
 #define __PLAYBACK_H
+void Playback_init(void);
+void Playback_cleanup(void);
 void Playback_playWithDelay(short* frame, int frameSize, long delayTime);
 #endif
