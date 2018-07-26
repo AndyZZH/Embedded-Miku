1770a504c541d0246fda687a347ccde0bbca79f5
Andy Zhang <hiandyzhang@gmail.com>
2018-07-25 17:57:39 -0700

    Add library

---
+++ libsamplerate/build_s/CTestTestfile.cmake
@@ -0,0 +1,22 @@
+# CMake generated Testfile for 
+# Source directory: /home/andy/Embedded-Miku/libsamplerate
+# Build directory: /home/andy/Embedded-Miku/libsamplerate/build_s
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(callback_hang_test "/home/andy/Embedded-Miku/libsamplerate/build_s/callback_hang_test")
+add_test(callback_test "/home/andy/Embedded-Miku/libsamplerate/build_s/callback_test")
+add_test(clone_test "/home/andy/Embedded-Miku/libsamplerate/build_s/clone_test")
+add_test(downsample_test "/home/andy/Embedded-Miku/libsamplerate/build_s/downsample_test")
+add_test(float_short_test "/home/andy/Embedded-Miku/libsamplerate/build_s/float_short_test")
+add_test(misc_test "/home/andy/Embedded-Miku/libsamplerate/build_s/misc_test")
+add_test(multi_channel_test "/home/andy/Embedded-Miku/libsamplerate/build_s/multi_channel_test")
+add_test(multichan_throughput_test "/home/andy/Embedded-Miku/libsamplerate/build_s/multichan_throughput_test")
+add_test(nullptr_test "/home/andy/Embedded-Miku/libsamplerate/build_s/nullptr_test")
+add_test(reset_test "/home/andy/Embedded-Miku/libsamplerate/build_s/reset_test")
+add_test(simple_test "/home/andy/Embedded-Miku/libsamplerate/build_s/simple_test")
+add_test(snr_bw_test "/home/andy/Embedded-Miku/libsamplerate/build_s/snr_bw_test")
+add_test(streaming_test "/home/andy/Embedded-Miku/libsamplerate/build_s/streaming_test")
+add_test(termination_test "/home/andy/Embedded-Miku/libsamplerate/build_s/termination_test")
+add_test(throughput_test "/home/andy/Embedded-Miku/libsamplerate/build_s/throughput_test")
+add_test(varispeed_test "/home/andy/Embedded-Miku/libsamplerate/build_s/varispeed_test")
