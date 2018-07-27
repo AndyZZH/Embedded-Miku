73ad10890ea7eae172ff9510f5ee1591a7e10cb6
Peter Tan <pj.t@outlook.com>
2018-07-26 18:17:31 -0700

    Implement a sleep in audio's main.c

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
-
+// #include <unistd.h>
+#include <time.h>
 
 #include "audio_recorder.h"
 #include "playback.h"
@@ -11,11 +12,15 @@ int main() {
 
     unsigned long frameSize = AudioRecorder_getFrameSize();
 
+    struct timespec reqtime;
+    reqtime.tv_sec=0;
+    reqtime.tv_nsec = 50 * 1000;
     while (1) {
         double *data = AudioRecorder_getNextAudioReading();
         if (data != NULL) {
-            Playback_playWithDelay(data, frameSize, 100);
+            Playback_playWithDelay(data, frameSize, 1000);
         }
+        nanosleep(&reqtime, NULL);
     }
 
     AudioRecorder_cleanup();
