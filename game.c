f188f041fde4826e6405866473e8b4cd31fde05d
RaytorHu <37994163+RaytorHu@users.noreply.github.com>
2018-07-26 21:10:26 -0700

    add the life count to game module

--- game.c
+++ game.c
@@ -13,19 +13,22 @@
 #define DEFAULT_DELAY_TIMEms 1000 // default delay time is 1s
 #define REACT_TIMEms 250 //default react time +- 0.25s
 #define DEFAULT_SCORE 0 //default score is 0
+#define DEFAULT_LIFE 4 // default chances before player lose
 
 // prototype
 
 static int score = DEFAULT_SCORE;
 static long long delayTime = DEFAULT_DELAY_TIMEms;
 static long long beatQueue[XBOX_NUM_TYPE];
-
+static int life = DEFAULT_LIFE;
+static bool timeoutQueue[XBOX_NUM_TYPE];
 
 void Game_init(void){
     srand(time(NULL));
     score_setDigit(score);
     for (int i = 0; i < XBOX_NUM_TYPE; i++){
         beatQueue[i] = 0;
+        timeoutQueue[i] = false;
     }
 }
 
@@ -55,6 +58,7 @@ void Game_EnqueueBeat(void){
         if (beatQueue[currentSlot] + delayTime < currentTime){
             // overdue, see it as an empty slot
             beatQueue[currentSlot] = currentTime;
+            timeoutQueue[currentSlot] = true;
             // call led dropping
             Display_generateComponent(currentSlot);
             break;
@@ -85,8 +89,31 @@ void Game_checkBeat(int button_id){
         score ++;
         score_setDigit(score);
         beatQueue[button_id] = 0;
+        timeoutQueue[currentSlot] = false;
 	printf("The score is %d.\n", score);
     }
+}
+
+static void decrease_life(){
+  life--;
+  //call turn off led
+  Display_decreaseLife();
+  if(life == 0){
+    //game over
+  }
+}
+
+/*
+ * Function:  Game_checkTimeout()
+ * --------------------
+ * Function to check if a slot is timeout
+ *
+ *  returns: void
+ */
+void Game_checkTimeout(int slot){
+  if(timeoutQueue[slot]){
+    decrease_life();
+  }
 }
 
 int Game_getScore(){
